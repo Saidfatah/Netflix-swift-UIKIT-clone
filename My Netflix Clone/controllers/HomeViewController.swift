0ae30574f65938e7237bb8319aae312fb8c03f0e
SaidFatah <said_designer@outlook.com>
2022-04-22 21:11:29 +0000

    [HomeFeed- screen] setup rows using UITableView and added cells the the rows using horizontal UiCollectionView

--- My Netflix Clone/controllers/HomeViewController.swift
+++ My Netflix Clone/controllers/HomeViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    var homeFeedTableView :UITableView = {
+        var _tableView = UITableView(frame: .zero, style: .grouped)
+        _tableView.register(CollectionTableViewCell.self, forCellReuseIdentifier: CollectionTableViewCell.indentifier)
 
+        return _tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTableView)
+        homeFeedTableView.dataSource = self
+        homeFeedTableView.delegate = self
+        homeFeedTableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 400))
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTableView.frame = view.bounds
+    }
+
+}
+
+extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        20
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionTableViewCell.indentifier, for: indexPath) as? CollectionTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
+    }
+    
 }
---
+++ My Netflix Clone/views/CollectionTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  CollectionTableViewCell.swift
+//  My Netflix Clone
+//
+//  Created by said fatah on 22/4/2022.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+    static let indentifier = "collection-view-table-cell"
+    
+    private let collectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 140, height: 200)
+        let collectionView = UICollectionView(frame: .zero , collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .blue
+        contentView.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+   
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+}
+
+extension CollectionTableViewCell : UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
